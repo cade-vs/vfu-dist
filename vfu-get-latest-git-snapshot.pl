61e6702381b3cd73d8d1727b0128d74f92834d81
Vladi Belperchinov-Shabanski <cade@bis.bg>
2020-08-08 14:51:13 +0300

    added-tool-for-pulling-latest-vfu-git-snapshot

---
+++ vfu-get-latest-git-snapshot.pl
@@ -0,0 +1,44 @@
+#!/usr/bin/perl
+use strict;
+
+my $dest = shift;
+
+die "need accessible target directory as 1st arg\n" unless $dest and chdir( $dest );
+
+clone_or_update( "https://github.com/cade-vs/vfu-dist.git" );
+chdir( 'vfu-dist' );
+clone_or_update( "https://github.com/cade-vs/vfu.git" );
+clone_or_update( "https://github.com/cade-vs/vslib.git" );
+clone_or_update( "https://github.com/cade-vs/vstring.git" );
+clone_or_update( "https://github.com/cade-vs/yascreen.git" );
+status( "done." );
+
+sub clone_or_update
+{
+  my $repo = shift;
+  
+  my $dir  = $1 if $repo =~ /([^\/]+)\.git$/;
+  
+  die "cannot figure dir name from repo [$repo]" unless $dir;
+
+  if( -e $dir )
+    {
+    status( "updating $repo" );
+    chdir( $dir );
+    system qq[ git pull origin master ];
+    chdir( '..' );
+    }
+  else
+    {
+    status( "cloning $repo" );
+    system qq[ git clone $repo ];
+    }  
+  
+  return 1;
+}
+
+sub status
+{
+  my $s = shift;
+  print( "\n" . ('-' x 16) . ' ' . $s . "\n" );
+}
